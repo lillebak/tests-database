d6e3821cbb32e480bb54bd71a64a3eaa471b0719
Jens Peter Lillebæk <jenspeter@lillebak.dk>
2022-08-26 19:18:24 +0200

    Test console project with sqlite EF6 using model first.

---
+++ EF6ModelFirstConsole/EF6ModelFirstConsole/DbContexts/OrderSystemContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SqlLiteInNET6.Entities;
+
+namespace SqlLiteInNET6.DbContexts
+{
+    public class OrderSystemContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=OrderSystemConsole.db");
+        }
+    }
+}
---
+++ EF6ModelFirstConsole/EF6ModelFirstConsole/Entities/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SqlLiteInNET6.Entities
+{
+    public class Customer
+    {
+        public Customer(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Address { get; set; }
+
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    }
+}
